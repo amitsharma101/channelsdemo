fa9af8f1099cf7f29d76bd8b3eeacd52fcc9b0b4
Ubuntu <ubuntu@ip-172-31-11-216.ap-south-1.compute.internal>
2021-07-13 14:35:31 +0000

    Commit from server

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hz0j*-&(=$7(io6yy00swgbb6u&q1_bs9yl12mwadn0+q=07zd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
